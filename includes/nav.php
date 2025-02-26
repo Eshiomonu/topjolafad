0ccdd64f7ab86295b87c1ea3d331f5bf6dc337a4
kadirisunday <kadirisunday212@gmail.com>
2025-02-26 16:49:06 +0100

    project setup

---
+++ includes/nav.php
@@ -0,0 +1,70 @@
+     <!-- Header Start -->
+     <header>
+       <div class="header-area header-transparent">
+         <div class="main-header ">
+           <div class="header-top d-none d-lg-block">
+             <div class="container-fluid">
+               <div class="col-xl-12">
+                 <div class="row d-flex justify-content-between align-items-center">
+                   <div class="header-info-left">
+                     <ul>
+                       <li>+(234) 803 510 8339</li>
+                       <li>info@topjolafad.com.ng</li>
+                       <li>Mon - Sat 8:00 - 17:30, Sunday - CLOSED</li>
+                     </ul>
+                   </div>
+                   <div class="header-info-right">
+                     <ul class="header-social">
+                       <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                       <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                       <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                       <li> <a href="#"><i class="fab fa-instagram"></i></a></li>
+                     </ul>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div class="header-bottom  header-sticky">
+             <div class="container-fluid">
+               <div class="row align-items-center">
+                 <!-- Logo -->
+                 <div class="col-xl-2 col-lg-2 col-md-1">
+                   <div class="logo">
+                     <!-- logo-1 -->
+                     <a href="index.html" class="big-logo"><img src="assets/img/logo/logo.png" alt=""></a>
+                     <!-- logo-2 -->
+                     <!-- <a href="index.html" class="small-logo"><img src="assets/img/logo/loder-logo.png" alt=""></a> -->
+                   </div>
+                 </div>
+                 <div class="col-xl-8 col-lg-8 col-md-8">
+                   <!-- Main-menu -->
+                   <div class="main-menu f-right d-none d-lg-block">
+                     <nav>
+                       <ul id="navigation">
+                         <li><a href="index.html">Home</a></li>
+                         <li><a href="about.html">About</a></li>
+                         <li><a href="project.html">Projects</a></li>
+                         <li><a href="services.html">Services</a></li>
+                         <li><a href="blog.html">Blog</a></li>
+                         <!-- <li><a href="contact.html">Contact</a></li> -->
+                       </ul>
+                     </nav>
+                   </div>
+                 </div>
+                 <div class="col-xl-2 col-lg-2 col-md-3">
+                   <div class="header-right-btn f-right d-none d-lg-block">
+                     <a href="contact.html" class="btn">Contact Now</a>
+                   </div>
+                 </div>
+                 <!-- Mobile Menu -->
+                 <div class="col-12">
+                   <div class="mobile_menu d-block d-lg-none"></div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+       <!-- Header End -->
+     </header>
